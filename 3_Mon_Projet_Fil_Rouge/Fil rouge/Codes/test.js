a79dceedb34ef1802110c4b4afd1d7967f215615
Toky Sigogneau <137786950+ReverieLanaya@users.noreply.github.com>
2023-11-19 22:13:31 +0100

    oral part 2
    added front code

---
+++ 3_Mon_Projet_Fil_Rouge/Fil rouge/Codes/test.js
@@ -0,0 +1,11 @@
+
+function afficherPopup() {
+    var popup = document.getElementById('maPopup');
+    popup.style.display = 'flex';
+  }
+  
+  function fermerPopup() {
+    var popup = document.getElementById('maPopup');
+    popup.style.display = 'none';
+  }
+
